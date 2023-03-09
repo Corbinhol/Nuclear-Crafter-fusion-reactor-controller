7c255d76638e5becfe99515367247e580be801bb
Corbinhol <84656556+Corbinhol@users.noreply.github.com>
2023-03-09 15:53:36 -0600

    Working On Setup.lua

--- Setup.lua
+++ Setup.lua
@@ -3,23 +3,43 @@ local component = require("component");
 local term = require("term");
 local version = "0.1";
 
-term.clear();
-print(string.rep("═", 80));
-print("Starting Reactor Controller Setup | " .. version);
-if filesystem.exists("/ReactorController/Controller.lua") then
-    print("Detected controller already on system...");
-    print("Would you like to uninstall the Controller first? [Y/n]")
-    local answer = io.read();
-    if answer == "n" or answer == "N" then
-        print("Closing Setup...");
-        os.sleep(1);
-        term.clear();
-        os.exit();
-    else
-        print("Uninstalling Reactor Controller...");
-        os.sleep(1);
-        filesystem.remove("/ReactorController")
-    end
+local args = {...};
+
+local uninstall = false;
+local forceInstall = false;
+
+for i,arg in ipairs(args) do
+    if arg == "-u" then uninstall = true; end
+    if arg == "-f" then forceInstall = true; end
 end
 
-print("Starting Setup")
+function run_uninstall()
+    print("Uninstalling Reactor Controller...");
+    os.sleep(1);
+    filesystem.remove("/ReactorController");
+end
+
+term.clear();
+print(string.rep("═", 80));
+if uninstall then 
+    run_uninstall()
+else
+    if component.isAvailable("nc_fusion_reactor") or forceInstall then
+        print("Starting Reactor Controller Setup | " .. version);
+        if filesystem.exists("/ReactorController/Controller.lua") then
+            print("Detected controller already on system...");
+            print("Would you like to uninstall the Controller first? [Y/n]")
+            local answer = io.read();
+            if answer == "n" or answer == "N" then
+                print("Closing Setup...");
+                os.sleep(1);
+                term.clear();
+                os.exit();
+            else
+                run_uninstall();
+            end
+        end
+    end
+    print("Starting Setup");
+end
+
