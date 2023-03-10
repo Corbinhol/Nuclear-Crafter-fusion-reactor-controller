45d21a2c12200ac68682b09578659660935086e9
Corbinhol <84656556+Corbinhol@users.noreply.github.com>
2023-03-10 03:28:55 -0600

    Added Uptime to display

--- Controller.lua
+++ Controller.lua
@@ -6,6 +6,7 @@ local event = require("event");
 local serialization = require("serialization");
 local term = require("term");
 local thread = require("thread");
+local computer = require("computer");
 local gpu = component.gpu
 local run = true;
 api = {};
@@ -31,11 +32,34 @@ function stop()
 
 end
 
+local function indexTime(timeInSeconds)
+    local numberOfSeconds = timeInSeconds % 60;
+    local numberOfMinutes = math.floor(timeInSeconds / 60);
+    local numberOfHours = math.floor(numberOfMinutes / 60);
+    numberOfMinutes = numberOfMinutes % 60;
+    local numberOfDays = math.floor(numberOfHours / 24);
+    numberOfHours = numberOfHours % 24;
+
+    if numberOfSeconds < 10 then numberOfSeconds = "" .. 0 .. numberOfSeconds; end
+    if numberOfMinutes < 10 then numberOfMinutes = "" .. 0 .. numberOfMinutes; end
+    if numberOfHours < 10 then numberOfHours = "" .. 0 .. numberOfHours; end
+    if numberOfDays < 10 then numberOfDays = "" .. 0 .. numberOfDays; end
+    local output = "";
+    if numberOfDays ~= "00" then output = output .. numberOfDays .. ":"; end
+    if numberOfHours ~= "00" then output = output .. numberOfHours .. ":"; end
+    output = output .. numberOfMinutes .. ":" .. numberOfSeconds;
+    return output;
+end
+
 function updateDisplay()
     while run do
     term.clear();
     gpu.set(1,1, string.rep("═", 80));
     gpu.set(2,2, "Reactor Controller [Version " .. version .. "]");
+    gpu.set(2,3, "Reactor Status: ");
+    gpu.set(20, 3, "Uptime: " .. indexTime(computer.uptime()));
+    gpu.set(2,4, "Autostart: ");
+
     end
 end
 display = thread.create(updateDisplay);
