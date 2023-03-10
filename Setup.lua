a6b0d03d0b2d3a435d765fd286a9daf3a5cf7e33
Corbinhol <84656556+Corbinhol@users.noreply.github.com>
2023-03-10 07:15:21 -0600

    Added Pastebin Option To Setup

--- Setup.lua
+++ Setup.lua
@@ -12,11 +12,18 @@ local args = {...};
 local uninstall = false;
 local forceInstall = false;
 local repair = false;
+local pastebin = false;
 
 for i,arg in ipairs(args) do
     if arg == "-u" then uninstall = true; end
     if arg == "-f" then forceInstall = true; end
     if arg == "-r" then repair = true; end
+    if arg == "-p" then pastebin = true; end
+end
+
+function pastebinInstall()
+    shell.execute("pastebin get wJCXfem8 /bin/Controller.lua"); --Download Controller
+    shell.execute("pastebin get KAR6jmr8 /home/FusionController/API.lua");
 end
 
 function run_uninstall()
@@ -40,7 +47,7 @@ else
         if filesystem.exists("/bin/Controller.lua") then
             if repair == false then
                 print("Detected controller already on system...");
-                print("Would you like to uninstall the Controller first? [Y/n]")
+                print("Would you like to uninstall the Controller first? [Y/n]");
                 local answer = io.read();
             else
                 answer = "Y"
@@ -54,10 +61,26 @@ else
                 run_uninstall();
             end
         end
-        print("Starting Install...");
-        shell.execute("mkdir /home/FusionController")
-        shell.execute("wget https://raw.githubusercontent.com/Corbinhol/Nuclear-Crafter-fusion-reactor-controller/main/Controller.lua /bin/Controller.lua -Q");
-        shell.execute("wget https://raw.githubusercontent.com/Corbinhol/Nuclear-Crafter-fusion-reactor-controller/main/Api.lua /home/FusionController/Api.lua -Q");
+        if pastebin then
+            if not forceInstall then
+                print("[WARNING] Pastebin Argument detected. Using pastebin is not the recommended form, since pastes aren't permanent. Would you like to continue [Y/n]");
+                local pastebinAnswer = io.read();
+            else
+                local pastebinAnswer = "y";
+            end
+            if pastebinAnswer == "n" or pastebinAnswer == "N" then
+                print("Closing Setup...");
+                os.sleep(1);
+                term.clear();
+                os.exit(); 
+            end
+            pastebinInstall();
+        else
+            print("Starting Install...");
+            shell.execute("mkdir /home/FusionController")
+            shell.execute("wget https://raw.githubusercontent.com/Corbinhol/Nuclear-Crafter-fusion-reactor-controller/main/Controller.lua /bin/Controller.lua -Q");
+            shell.execute("wget https://raw.githubusercontent.com/Corbinhol/Nuclear-Crafter-fusion-reactor-controller/main/Api.lua /home/FusionController/Api.lua -Q");
+        end
         shell.execute("rm Setup.lua");
         print("Finished Installing. Press any key to restart.");
         io.read();
